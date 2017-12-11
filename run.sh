b32f8480b22816e5fe1dba98d87bb463b43f5a85
meirshal <meirshal@gmail.com>
2017-12-11 10:56:57 +0200

    modifications

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 entrypoint=${entrypoint='https://dev1-oh.getvim.com/ssoEntrypoint'}
 
-node app.js --acs $entrypoint --aud 'aud' --encCert ./sp-signing.cert --encryptionPublicKey ./sp-signing.pub
+node app.js --acs $entrypoint --aud 'aud' --encCert ./sp.cert --encryptionPublicKey ./sp.pub
